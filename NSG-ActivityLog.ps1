24008885928ffdb72dfd7817567abc6c57c09805
Kosta S <43343162+KostaS10@users.noreply.github.com>
2020-04-16 12:28:56 +0200

    Update NSG-ActivityLog.ps1

--- NSG-ActivityLog.ps1
+++ NSG-ActivityLog.ps1
@@ -42,22 +42,70 @@ $enddate = [string]$lastMonthNumber + "/" + $lastDay + "/" + $lastMonthYear
 
 #Defining query and commands
 $query = "AzureActivity
-| where OperationNameValue startswith ""Microsoft.Network/networkSecurityGroups/securityRules"" 
-and OperationName != ""'audit' Policy action."" 
-and ActivityStatus == ""Succeeded""   
+| where OperationNameValue startswith ""Microsoft.Network/networkSecurityGroups/securityRules""
+and ResourceGroup == "$resourceGroup"
+and OperationName != ""'audit' Policy action.""
 and TimeGenerated >= datetime($startdate)
 and TimeGenerated <= datetime($enddate)
 | sort by TimeGenerated desc"
 
-$workspaceId = (Get-AzOperationalInsightsWorkspace -Name $workspaceName -ResourceGroupName $rgName).CustomerId.Guid
+$workspaceId = (Get-AzOperationalInsightsWorkspace -Name "$workspacename" -ResourceGroupName "resourceGroup").CustomerId.Guid
 
 
 #Executing LogAnalytics query and parsing results
-$result = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $query
-$resultsArray = [System.Linq.Enumerable]::ToArray($result.Results) 
+$results = Invoke-AzOperationalInsightsQuery -WorkspaceId $workspaceId -Query $query
+$resultsArray = [System.Linq.Enumerable]::ToArray($results.Results)
+
+$resulting=@()
+$item1=@()
+
+foreach($resultsArrayItem in $resultsArray){
+    $item1 = $resultsArrayItem.Properties
+
+    if($item1 -notlike "*responseBody*"){
+    
+        $provisioningState = ""
+        $description = ""
+        $protocol = ""
+        $sourcePortRange = ""
+        $sourceAddressPrefix = ""
+        $access = ""
+        $priority = ""
+        $direction = ""
+        $sourcePortRanges = ""
+        $destinationPortRanges = ""
+        $sourceAddressPrefixes = ""
+        $destinationAddressPrefixes = ""
+    }
+    else{
+        
+
+        $item2 = $item1 | ConvertFrom-Json
+        $item3 = $item2.responseBody | ConvertFrom-Json
+        $provisioningState = [string]$item3.properties.provisioningState
+        $description = [string]$item3.properties.description
+        $protocol = [string]$item3.properties.protocol
+        $sourcePortRange = [string]$item3.properties.sourcePortRange
+        $sourceAddressPrefix = [string]$item3.properties.sourceAddressPrefix
+        $access = [string]$item3.properties.access
+        $priority = [string]$item3.properties.priority
+        $direction = [string]$item3.properties.direction
+        $sourcePortRanges = [string]$item3.properties.sourcePortRanges
+        $destinationPortRanges = [string]$item3.properties.destinationPortRanges
+        $sourceAddressPrefixes = [string]$item3.properties.sourceAddressPrefixes
+        $destinationAddressPrefixes = [string]$item3.properties.destinationAddressPrefixes
+    }
+    $resulting += $resultsArrayItem | select TimeGenerated, CallerIpAddress, OperationNameValue, Caller, EventSubmissionTimestamp, OperationName, Resource, 
+    @{n='Description';e={$description}}, @{n='ProvisioningState';e={$provisioningState}}, @{n='Protocol';e={$protocol}}, @{n='SourcePortRange';e={$sourcePortRange}},
+    @{n='SourceAddressPrefix';e={$sourceAddressPrefix}}, @{n='Access';e={$access}}, @{n='Priority';e={$priority}}, @{n='Direction';e={$direction}},
+    @{n='SourcePortRanges';e={$sourcePortRanges}}, @{n='DestinationPortRanges';e={$destinationPortRanges}}, @{n='SourceAddressPrefixes';e={$sourceAddressPrefixes}},
+    @{n='DestinationAddressPrefixes';e={$destinationAddressPrefixes}}
+}
+
+
 
 $path =  "$env:TEMP\NSG_ActivityLog" + "_" + $lastMonthNumber + "_" + $lastMonthYear + ".csv"
-$resultsArray | Export-Csv $path
+$resulting | Export-Csv -Path $path
 
 #Set storage context and date
 
